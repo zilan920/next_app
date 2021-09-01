092bb956890f28f2ce8c8a0930f7e43a2833303d
zihan.fang <zihan.fang@bybit.com>
2021-09-01 16:00:07 +0800

    1

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          难道我不牛逼吗
         </p>
 
         <div className={styles.grid}>
